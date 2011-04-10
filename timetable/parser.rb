362c524f9eb9a1859339b3b43dcc61b9c8b12bfb
Leo Cassarani <leonardo.cassarani@gmail.com>
2011-04-10 13:22:59 +0100

    Parsers can now break down every event into its attributes

--- timetable/parser.rb
+++ timetable/parser.rb
@@ -27,18 +27,26 @@ module Timetable
       find_week_start
       find_cells
       parse_cells
+      @calendar
     end
+
+    private
 
     # Retrieves the string that indicates when the week that the
     # table represents begins, and saves it as a DateTime object
     def find_week_start
       @doc ||= Hpricot(input)
-      start_text = @doc.at("body/font").inner_html
+      # Retrieve the first occurrence of a stand-alone <font> tag,
+      # which happens to be the week start date. Return if nil,
+      # which would normally indicate bad input, e.g. empty string.
+      tag = @doc.at("body/font")
+      return if tag.nil?
+
+      start_text = tag.inner_html
       # Retrieve text of the form "Monday 11 October, 2010"
       start_text = start_text.scan(/\w+ \d{1,2} \w+, \d{4}$/).first
       # Parse the resulting text into a DateTime object
       @week_start = DateTime.strptime(start_text, "%a %d %b, %Y")
-      @week_start = @week_start.advance_hours(MORNINGSTART)
     end
     
     # Retrieves an array of the textual contents of the table cells
@@ -48,11 +56,12 @@ module Timetable
       @cells.map! { |node| node.inner_html }
     end
     
+    # Iterates over the array of table cells and puts together an
+    # Icalendar::Calendar object with all the events it can find
     def parse_cells
       @calendar = Calendar.new
+      day = time = 0
       
-      day = 0
-      time = 0
       @cells.each do |cell|
         # Reset day/time counts if this is the horizontal heading ("0900")
         if cell =~ /^(\d{2})00$/
@@ -62,8 +71,13 @@ module Timetable
         end
         
         # Only deal with the cell if it's not empty or just a newline
-        if cell !~ /^(<br \/>)?$/
-          # Parse the cell contents...
+        unless cell.empty? or cell == "<br />"
+          lines = cell.split("<br />").delete_if { |s| s.empty? }
+          # Each event is made up of two lines, so we take them both
+          lines.each_slice(2) do |event|
+            title, extra = event
+            type, attendees, location = extra.split(" / ")
+          end
         end
         
         day += 1

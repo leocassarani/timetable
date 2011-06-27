f154cda000a885bd1411c63000e28de602bc0c2d
Leo Cassarani <leonardo.cassarani@gmail.com>
2011-06-27 08:25:56 +0100

    Congigure RSpec to always output colours

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,3 +2,7 @@ $LOAD_PATH << File.expand_path(File.join(File.dirname(__FILE__), "..", "lib"))
 require 'timetable'
 
 ENV['RACK_ENV'] = 'test'
+
+RSpec.configure do |c|
+  c.color_enabled = true
+end

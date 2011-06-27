192ebfb76cdc59fc1eec4ac284d84d55e731ac7f
Leo Cassarani <leonardo.cassarani@gmail.com>
2011-06-27 13:27:48 +0100

    Start writing specs for the new Parser

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -1,215 +1,116 @@
 require 'spec_helper'
 
 describe Timetable::Parser do
-  def read_sample_data(filename)
+  def sample_data(filename)
     filepath = File.join(File.dirname(__FILE__), "data/#{filename}")
     File.read(filepath)
   end
 
-  before :each do
-    @parser = Timetable::Parser.new
-  end
+  describe "#parse" do
+    let(:delegate) { double('delegate').as_null_object }
+    let(:parser) { Timetable::Parser.new(delegate) }
 
-  context "given no input" do
-    it "returns nil" do
-      @parser.parse.should == nil
-    end
-  end
-
-  context "given nil input" do
-    it "returns nil" do
-      @parser.input = nil
-      @parser.parse.should == nil
-    end
-  end
-
-  it "can parse several different files" do
-    @parser.input = read_sample_data("single.html")
-    first = @parser.parse
-    @parser.input = read_sample_data("single_repeating.html")
-    second = @parser.parse
-    first.should_not == second
-  end
-
-  context "given an input" do
-    it "returns a calendar" do
-      @parser.input = read_sample_data("single.html")
-      @parser.parse.should be_a_kind_of Icalendar::Calendar
-    end
-  end
-
-  context "given an empty string" do
-    it "returns an empty calendar" do
-      @parser.input = ""
-      cal = @parser.parse
-      cal.should be_a_kind_of Icalendar::Calendar
-      cal.events.should be_empty
-    end
-  end
-
-  context "given an empty timetable" do
-    it "returns an empty calendar" do
-      @parser.input = read_sample_data("empty.html")
-      cal = @parser.parse
-      cal.events.should be_empty
-    end
-  end
-
-  context "given all possible inputs for the location field" do
-    it "produces a correct list of rooms" do
-      @parser.input = read_sample_data("rooms.html")
-      events = @parser.parse.events
-
-      events.sort { |a, b| a.start <=> b.start }
-      locations = events.map(&:location)
-
-      empty, textual, numeric_single, numeric_multiple, mix = locations
-      empty.should be_empty
-      textual.should == "G16 Sir Alexander Flemming Bldg"
-      numeric_single.should == "Room 308"
-      numeric_multiple.should == "Rooms 308, 343, 344"
-      mix.should == "Clore Lecture Theatre, Room 343"
-    end
-  end
-
-  context "given a single event" do
     before :each do
-      @parser.input = read_sample_data("single.html")
-      @result = @parser.parse
+      # The parser will send messages only if the delegate responds to them
+      delegate.stub!(:respond_to).and_return(true)
     end
 
-    it "creates one event only" do
-      @result.should have(1).events
+    it "instantly terminates given no input" do
+      delegate.should_not_receive(:parsing_ended)
+      parser.parse(nil)
     end
 
-    it "gives the correct attributes to the event" do
-      event = @result.events.first
-      event.summary.should == "Programming (Lecture)"
-      event.description.should == "ajf"
-      event.location.should == "Room 308"
-      event.start.should == DateTime.civil(2010, 10, 6, 10)
-      event.end.should == event.start.advance(:hours => 1)
-    end
-  end
+    context "given an empty timetable" do
+      let(:timetable) { sample_data("empty.html") }
 
-  context "given a single event that occurs five times" do
-    before :each do
-      @parser.input = read_sample_data("single_repeating.html")
-      @result = @parser.parse
-    end
+      it "parses weeks range and dates then terminates" do
+        delegate.should_receive(:process_week_range)
+        delegate.should_receive(:process_week_start)
+        delegate.should_receive(:process_week_end)
+        delegate.should_receive(:parsing_ended)
 
-    it "creates five distinct events" do
-      @result.should have(5).events
-    end
-
-    it "creates events with identical attributes" do
-      events = @result.events
-
-      first = events.shift
-      summary = first.summary
-      description = first.description
-      location = first.location
-      duration = first.end - first.start
-
-      events.each do |event|
-        event.summary.should == summary
-        event.description.should == description
-        event.location.should == location
-        (event.end - event.start).should == duration
+        parser.parse(timetable)
       end
     end
 
-    it "creates events that are one week apart" do
-      events = @result.events
-      first = events.shift
-      events.inject(first.start) do |date, event|
-        event.start.should == date.advance(:weeks => 1)
-        event.start
-      end
-    end
-  end
+    context "given a timetable with a single event" do
+      let(:timetable) { sample_data("single.html") }
 
-  context "given two events in the same timeslot" do
-    before :each do
-      @parser.input = read_sample_data("multiline.html")
-      @result = @parser.parse
-    end
-
-    it "creates two events" do
-      @result.should have(2).events
-    end
-
-    it "creates the events one week apart" do
-      events = @result.events
-      first, second = events.sort { |a, b| a.start <=> b.start }
-      second.start.should == first.start.advance(:weeks => 1)
-    end
-  end
-
-  context "given an event spanning two consecutive timeslots" do
-    before :each do
-      @parser.input = read_sample_data("multislot.html")
-      @result = @parser.parse
-    end
-
-    it "creates one event only" do
-      @result.should have(1).events
-    end
-
-    it "creates a two-hour event" do
-      event = @result.events.first
-      event.end.should == event.start.advance(:hours => 2)
-    end
-  end
-
-  context "given a repeating event that exceeds the range of the calendar" do
-    before :each do
-      @parser.input = read_sample_data("out_of_range.html")
-      @result = @parser.parse
-    end
-
-    it "only creates the events that occur on weeks covered by the calendar" do
-      @result.should have(5).events
-    end
-  end
-end
-
-# String monkey patches
-describe String do
-  describe "#integer?" do
-    context "given an unsigned nonnegative integer" do
-      let(:inputs) { %w[0 42 123] }
-
-      it "returns true" do
-        inputs.each do |input|
-          input.integer?.should be_true
-        end
+      it "calls process_event once with the event data" do
+        event = {
+          :day => 2,
+          :timeslot => 1,
+          :name => "Programming",
+          :type => "LEC",
+          :weeks => 1..1,
+          :attendees => ["ajf"],
+          :locations => ["308"]
+        }
+        delegate.should_receive(:process_event).with(event)
+        parser.parse(timetable)
       end
     end
 
-    context "given any non-integer input" do
-      let(:inputs) { [""] + %w[+123 -123 123b4con bacon] }
+    context "given a timetable with a single repeating event" do
+      let(:timetable) { sample_data("single_repeating.html") }
 
-      it "returns false" do
-        inputs.each do |input|
-          input.integer?.should be_false
-        end
+      it "calls process_event once with the event data" do
+        event = {
+          :day => 2,
+          :timeslot => 1,
+          :name => "Programming",
+          :type => "LEC",
+          :weeks => 2..6,
+          :attendees => ["ajf"],
+          :locations => ["308"]
+        }
+        delegate.should_receive(:process_event).with(event)
+        parser.parse(timetable)
       end
     end
-  end
 
-  describe ".pluralize" do
-    context "given count of 1" do
-      it "uses the singular" do
-        String.pluralize(1, "bacon").should == "bacon"
-        String.pluralize(1, "mouse", "mice").should == "mouse"
+    context "given two events in the same timeslot" do
+      let(:timetable) { sample_data("multiline.html") }
+
+      it "calls process_event twice with different data" do
+        first = {
+          :day => 3,
+          :timeslot => 5,
+          :name => "Discrete Mathematics I",
+          :type => "LEC",
+          :weeks => 3..3,
+          :attendees => ["yg"],
+          :locations => ["308"]
+        }
+        second = first.merge (
+          :name => "Mathematical Methods",
+          :type => "LEC",
+          :weeks => 2..2,
+          :attendees => ["jb"]
+        )
+        delegate.should_receive(:process_event).with(first)
+        delegate.should_receive(:process_event).with(second)
+        parser.parse(timetable)
       end
     end
 
-    context "given count different from 1" do
-      it "uses the plural" do
-        String.pluralize(0, "bacon").should == "bacons"
-        String.pluralize(42, "mouse", "mice").should == "mice"
+    context "given the same event spanning consecutive timeslots" do
+      let(:timetable) { sample_data("multislot.html") }
+
+      it "calls process_event twice with a different timeslot value" do
+        first = {
+          :day => 2,
+          :timeslot => 0,
+          :name => "Programming",
+          :type => "LEC",
+          :weeks => 2..2,
+          :attendees => ["ajf"],
+          :locations => ["308"]
+        }
+        second = first.merge(:timeslot => 1)
+        delegate.should_receive(:process_event).with(first)
+        delegate.should_receive(:process_event).with(second)
+        parser.parse(timetable)
       end
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,8 @@
-$LOAD_PATH << File.expand_path(File.join(File.dirname(__FILE__), "..", "lib"))
 require 'timetable'
 
 ENV['RACK_ENV'] = 'test'
 
-RSpec.configure do |c|
-  c.color_enabled = true
+RSpec.configure do |config|
+  config.color_enabled = true
+  config.mock_with :rspec
 end
